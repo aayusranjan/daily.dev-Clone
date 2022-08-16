97ca7c9ba69abcd550fbb6908a06b1ccc4f27389
AYUSH RANJAN <63737303+aayusranjan@users.noreply.github.com>
2022-08-16 20:44:32 +0530

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import Comment from "./comment-popup/Comment";
+import LikeUpdate from './like-update/likeUpdate'
+
 import "./Post.css";
 const Post = ({ post: { title, body, imgUrl, author }, index }) => {
   return (
     <div className="post-container">
       <h1 className="heading">{title}</h1>
       <img className="image" src={imgUrl} alt="post" />
-      <p>{body}</p>
-      <div className="info">
+      {/* <p className="text-content">{body}</p> */}
+      {/* <div className="info">
         <h4>Written by: {author}</h4>
+      </div> */}
+      <div className="icons">
+        <LikeUpdate />
+        <Comment />
+        <BookmarkBorderIcon />
       </div>
     </div>
   );
