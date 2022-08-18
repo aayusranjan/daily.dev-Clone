9932c2ec5f7d991fe9c69b1b38e78f447b1e2361
AYUSH RANJAN <63737303+aayusranjan@users.noreply.github.com>
2022-08-18 22:39:03 +0530

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import Comment from "./comment-popup/Comment";
 import LikeUpdate from './like-update/likeUpdate'
@@ -6,20 +7,26 @@ import LikeUpdate from './like-update/likeUpdate'
 import "./Post.css";
 const Post = ({ post: { title, body, imgUrl, author }, index }) => {
   return (
-    <div className="post-container">
-      <h1 className="heading">{title}</h1>
-      <img className="image" src={imgUrl} alt="post" />
-      {/* <p className="text-content">{body}</p> */}
-      {/* <div className="info">
+    <>
+      <div className="post-container">
+        <Link to="/Content">
+          <h1 className="heading">{title}</h1>
+          <img className="image" src={imgUrl} alt="post" />
+        </Link>
+        {/* <p className="text-content">{body}</p> */}
+        {/* <div className="info">
         <h4>Written by: {author}</h4>
       </div> */}
-      <div className="icons">
-        <LikeUpdate />
-        <Comment />
-        <BookmarkBorderIcon />
+        <div className="icons">
+          <LikeUpdate />
+          <Comment />
+          <BookmarkBorderIcon />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
 export default Post;
+
+ 
